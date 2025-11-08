db94cfdc99f4b760e9be9cffdf75189f44816292
Github@123_ <shreyashkhodepatil@gmail.com>
2025-11-08 11:34:35 +0530

    Enviorement Variable Added

--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-  private apiUrl = 'https://fakestoreapi.com/products';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
